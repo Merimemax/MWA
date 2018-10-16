101a1d908368d81505d9ac69b28023f18e4ff785
Merih <merimemax@gmail.com>
2018-10-15 23:27:46 -0500

    lab

--- Lab_1/ECMAScript6/removeDuplicates/withEs6Features.js
+++ Lab_1/ECMAScript6/removeDuplicates/withEs6Features.js
@@ -1,21 +1,21 @@
 
-let x=[5,6,5,4,3,4,2,1,1]
+let x = [5, 6, 5, 4, 3, 4, 2, 1, 1]
 
 //ES6 features
-function removeDuplicates(a){
-    
-     const m=a.reduce(function(b,e,i){
-if(!(b.includes(e)))
-b.push(e);
-return b;
+function removeDuplicates(a) {
 
-},[]);
-    
+    const m = a.reduce(function (b, e, i) {
+        if (!(b.includes(e)))
+            b.push(e);
+        return b;
+
+    }, []);
+
     return m;
-    
+
 }
 
-let newArr=removeDuplicates(x);
+let newArr = removeDuplicates(x);
 console.log(newArr);
 
 
