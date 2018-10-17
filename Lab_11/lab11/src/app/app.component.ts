3da70fc8dc76004936452d441004060683b09b66
Merih <merimemax@gmail.com>
2018-10-16 19:01:43 -0500

    lab 11 Mr. Counter

--- Lab_11/lab11/src/app/app.component.ts
+++ Lab_11/lab11/src/app/app.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  {{titleMWA}}
+    <app-counter (eventClick)="updateCounter($event)" [counterValue]='counter'></app-counter>
+  `,
+
+  styles: []
 })
 export class AppComponent {
   title = 'lab11';
+  titleMWA = 'MWA Lab 11';
+
+  counter = 5;
+  updateCounter(num) {
+    this.counter += num;
+  }
+
+
 }
